f32d2f1ce95434f127b9e9dde3b00f670c21212b
Nenew12 <new_ton12@hotmail.com>
2013-12-09 12:03:00 +0700

    show product image

--- view/product_index.php
+++ view/product_index.php
@@ -1,11 +1,13 @@
-<h1><?php echo "product_heading" ?></h1>
 <html>
     <table>
-        
             <?php
-            while($row = $productList->fetch()) {
-                    echo "<tr><td>".$row['name']."</td></tr>";
-                    echo "<tr><td>".$row['price'].  "</td></tr>";
+            while($row = $productList->fetch()) { 
+                    echo "<ul><li>".$row['name']."</li>";
+                    echo "<li>".$row['price'].  "</li></ul>";
+                    echo "<ul><li><img src= content/image/product/".$row['item_no']."_1.png height='24' width='24'  /> </li>";
+                    echo "<li><img src= content/image/product/".$row['item_no']."_2.png  height='24' width='24'/> </li>";
+                    echo "<li><img src= content/image/product/".$row['item_no']."_3.png  height='24' width='24'/> </li></ul>";
+                    
             }
     ?>
 
