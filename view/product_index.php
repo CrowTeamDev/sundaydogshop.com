f497e0c5a046dfca0a550bf159e149ab580240f3
Nenew12 <new_ton12@hotmail.com>
2013-12-17 11:14:44 +0700

    add Large image

--- view/product_index.php
+++ view/product_index.php
@@ -3,29 +3,21 @@
     <table>
             <?php
             while($row = $productList->fetch()) { 
-                    echo "<ul><li>".$row['name']."</li>";
-                    echo "<li>".$row['price'].  "</li></ul>";
-                    echo "<img class='productImage' src= http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/small/image1.png  id=".$row['item_no']."_1 data-zoom-image=http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/large/image1.jpg >";
-//                    echo "<ul  ><li><img src= content/image/product/".$row['item_no']."_1.png height='24' width='24' > </li>";
-//                    echo "<li><img src= content/image/product/".$row['item_no']."_2.png  height='24' width='24' id=".$row['item_no']."_2  data-zoom-image='content/image/product/".$row['item_no']."_2.png' /> </li>";
-//                    echo "<li><img src= content/image/product/".$row['item_no']."_3.png  height='24' width='24' id=".$row['item_no']."_3  data-zoom-image='content/image/product/".$row['item_no']."_3.png' /> </li></ul>";
-                    echo "<div id=".$row['item_no'].">";   
-                    echo "<a href='#' data-image='http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/small/image1.png' data-zoom-image='http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/large/image1.jpg'>";
-                    echo "   <img id='img_01' src='http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/small/image1.png' height='50' width='50'/>";
+                    echo "<ol><li>".$row['name']."</li>";
+                    echo "<li>".$row['price'].  "</li></ol>";
+                    echo "<img class='productImage".$row['item_no']."' src= 'content/image/product/".$row['item_no']."_1.png'  id=".$row['item_no']."_1 data-zoom-image='content/image/product/".$row['item_no']."_1Large.png'  width='350px'  height='350px' >";
+                    echo "<div id=".$row['item_no']." class='thumbnail'>";   
+                    echo "<a href='#' data-image='content/image/product/".$row['item_no']."_1.png' data-zoom-image='content/image/product/".$row['item_no']."_1Large.png'>";
+                    echo "   <img id='".$row['item_no']."_01' src='content/image/product/".$row['item_no']."_1.png'/>";
+                    echo "</a>";
+                    echo "<a href='#' data-image='content/image/product/".$row['item_no']."_2.png' data-zoom-image='content/image/product/".$row['item_no']."_2Large.png'>";
+                    echo "   <img id='".$row['item_no']."_01' src='content/image/product/".$row['item_no']."_2.png'/>";
+                    echo "</a>";
+                     echo "<a href='#' data-image='content/image/product/".$row['item_no']."_3.png' data-zoom-image='content/image/product/".$row['item_no']."_3Large.png'>";
+                    echo "   <img id='".$row['item_no']."_01' src='content/image/product/".$row['item_no']."_3.png'/>";
                     echo "</a>";
 
-                    echo "<a href='#' data-image='http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/small/image2.png' data-zoom-image='http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/large/image2.jpg'>";
-                    echo "  <img id='img_01' src='http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/small/image2.png' height='50' width='50'/>";
-                    echo "</a>";
-
-                    echo "<a href='#' data-image='http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/small/image3.png' data-zoom-image='http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/large/image3.jpg'>";
-                    echo "  <img id='img_01' src='http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/small/image3.png' height='50' width='50'/>";
-                    echo "</a>";
-
-                    echo "<a href='#' data-image='http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/small/image4.png' data-zoom-image='http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/large/image4.jpg'>";
-                    echo "  <img id='img_01' src='http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/small/image4.png' height='50' width='50'/>";
-                    echo "</a>";
-
+             
                   echo "</div>";
             }
     ?>
@@ -35,13 +27,23 @@
     <script>
        // $(".productImage").elevateZoom();
         
-        $(".productImage").elevateZoom({gallery:'1', cursor: 'pointer', galleryActiveClass: 'active', imageCrossfade: true}); 
+        $(".productImage1").elevateZoom({gallery:'1', cursor: 'pointer', galleryActiveClass: 'active', imageCrossfade: true}); 
+        $(".productImage2").elevateZoom({gallery:'2', cursor: 'pointer', galleryActiveClass: 'active', imageCrossfade: true}); 
+
+
 
         //pass the images to Fancybox
-        $(".productImage").bind("click", function(e) {  
-          var ez =   $('.productImage').data('elevateZoom');	
+        $(".productImage1").bind("click", function(e) {  
+          var ez =   $('.productImage1').data('elevateZoom');	
                 $.fancybox(ez.getGalleryList());
           return false;
         });
+        
+        
+        
+        
+        function zoom(){
+            
+        }
     </script>
 </html>
