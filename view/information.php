81f5481cfea4dd7a5761741640d823848d4e2220
PeaceS <p.saravudecha@gmail.com>
2014-01-14 20:36:19 +0700

    Done About us page

--- view/information.php
+++ view/information.php
@@ -1,7 +1,5 @@
-<?php
-
-echo $info;
-
-?>
 <script src="js/views/information.js" type="text/javascript"></script>
+<div id="info_main">
+    <?php echo $info; ?>
+</div>
 <?php echo "<input type='hidden' id='page' value='".$page."' />"; 
