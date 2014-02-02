b3d2b6437017317a18a3cffb50f67fac762b13c8
Kongrath Suankaewmanee <kongrath.jojo@gmail.com>
2014-02-03 01:53:00 +0700

    update shop.js file

--- js/views/shop.js
+++ js/views/shop.js
@@ -17,18 +17,18 @@ $(function(){
     });
     
     $('.pagination-groupList').pagination({
-            items: 1,   // Total number of items that will be used to calculate the pages.
-            itemsOnPage: 1, // Number of items displayed on each page.
-            pages:$('#viewHigh').val(),   // If specified, items and itemsOnPage will not be used to calculate the number of pages.
-            displayedPages:5, // How many page numbers should be visible while navigating. Minimum allowed: 3 (previous, current & next)
-            edges:2,    // How many page numbers are visible at the beginning/ending of the pagination.
-            currentPage: $('#viewIndex').val(), // Which page will be selected immediately after init.
-            hrefTextPrefix: "shop?gb="+$('#gb').val()+"&viewIndex=", // A string used to build the href attribute, added before the page number.
-            hrefTextSuffix: '', // Another string used to build the href attribute, added after the page number.
-            prevText: "Prev", // Text to be display on the previous button.
-            nextText: "Next", // Text to be display on the next button.
-            cssStyle: "light-theme", // The class of the CSS theme.
-            selectOnClick: true, // Set to false if you don't want to select the page immediately after click.
+        items: 1,   // Total number of items that will be used to calculate the pages.
+        itemsOnPage: 1, // Number of items displayed on each page.
+        pages:$('#viewHigh').val(),   // If specified, items and itemsOnPage will not be used to calculate the number of pages.
+        displayedPages:5, // How many page numbers should be visible while navigating. Minimum allowed: 3 (previous, current & next)
+        edges:2,    // How many page numbers are visible at the beginning/ending of the pagination.
+        currentPage: $('#viewIndex').val(), // Which page will be selected immediately after init.
+        hrefTextPrefix: "shop?gb="+$('#gb').val()+"&viewIndex=", // A string used to build the href attribute, added before the page number.
+        hrefTextSuffix: '', // Another string used to build the href attribute, added after the page number.
+        prevText: "Prev", // Text to be display on the previous button.
+        nextText: "Next", // Text to be display on the next button.
+        cssStyle: "light-theme", // The class of the CSS theme.
+        selectOnClick: true, // Set to false if you don't want to select the page immediately after click.
     });
     $('.pagination-productList').pagination({
         items: 1,
