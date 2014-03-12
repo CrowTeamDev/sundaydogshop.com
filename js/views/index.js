76d8305d68395b5247aa3e519f5bf50f3bdfdb36
PeaceS <p.saravudecha@gmail.com>
2014-03-12 16:12:17 +0700

    Clean some code on index.php, order of load js, and set interval pause slide

--- js/views/index.js
+++ js/views/index.js
@@ -124,7 +124,13 @@ $(document).ready(function(){
             $(this).fadeOut(300, function(){
                 $('#navigation_bar').addClass('page_load').fadeIn(2500);
                 $('#top_menu, footer').addClass('page_load').fadeIn(500);
-                stopBackground();
+                $(".load-item").hide();
+                var pauseSlide = setInterval(function(){
+                    if (vars.is_paused)
+                        clearInterval(pauseSlide);
+                    if (!vars.in_animation && !vars.is_paused)
+                        api.playToggle();
+                }, 1000);
             }); 
         });
         $('#navigation_bar #main_menu li, footer label').not('#copyright').mouseover(function(){
@@ -172,57 +178,50 @@ $(document).ready(function(){
 
 });
 $(window).load(function(){
-     var dir = directory.get();
-     if(dir === "")
+     if(directory.get() === "")
          startBackground();
-     else
-         stopBackground();
+     
+    function startBackground(){	
+        var url_path = $('#local_path').val();
+        $.supersized({
+            slideshow : 1,              // Slideshow on/off
+            autoplay : 1,               // Slideshow starts playing automatically
+            start_slide : 1,            // Start slide (0 is random)
+            stop_loop : 0,              // Pauses slideshow on last slide
+            random : 0,                 // Randomize slide order (Ignores start slide)
+            slide_interval : 5000,      // Length between transitions
+            transition : 1,             // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+            transition_speed : 4000,    // Speed of transition
+            new_window : 1,             // Image links open in new window/tab
+            pause_hover : 0,            // Pause slideshow on hover
+            keyboard_nav : 0,           // Keyboard navigation on/off
+            performance : 0,            // 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
+            image_protect : 1,          // Disables image dragging and right click with Javascript
+            min_width : 0,              // Min width allowed (in pixels)
+            min_height : 0,             // Min height allowed (in pixels)
+            vertical_center : 1,        // Vertically center background
+            horizontal_center : 1,      // Horizontally center background
+            fit_always : 0,             // Image will never exceed browser width or height (Ignores min. dimensions)
+            fit_portrait : 1,           // Portrait images will not exceed browser height
+            fit_landscape : 0,          // Landscape images will not exceed browser width					
+            slide_links : 'blank',      // Individual links for each slide (Options: false, 'num', 'name', 'blank')
+            thumb_links : 1,            // Individual thumb links for each slide
+            thumbnail_navigation : 1,   // Thumbnail navigation
+            slides : [                  // Slideshow Images
+                {image : url_path + '/content/image/background/image_1.jpg'},
+                {image : url_path + '/content/image/background/image_2.jpg'},
+                {image : url_path + '/content/image/background/image_3.jpg'},  
+                {image : url_path + '/content/image/background/image_4.jpg'},  
+                {image : url_path + '/content/image/background/image_5.jpg'},  
+                {image : url_path + '/content/image/background/image_6.jpg'},  
+                {image : url_path + '/content/image/background/image_7.jpg'},  
+                {image : url_path + '/content/image/background/image_8.jpg'},  
+                {image : url_path + '/content/image/background/image_9.jpg'},  
+                {image : url_path + '/content/image/background/image_10.jpg'},  
+                {image : url_path + '/content/image/background/image_11.jpg'}
+            ],
+            progress_bar : 1,           // Timer for each slide							
+            mouse_scrub : 0
+        });
+    }
 });
-
-function startBackground(){			
-    var url_path = $('#local_path').val();
-    $.supersized({
-        slideshow : 1,              // Slideshow on/off
-        autoplay : 1,               // Slideshow starts playing automatically
-        start_slide : 1,            // Start slide (0 is random)
-        stop_loop : 0,              // Pauses slideshow on last slide
-        random : 0,                 // Randomize slide order (Ignores start slide)
-        slide_interval : 5000,      // Length between transitions
-        transition : 1,             // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-        transition_speed : 4000,    // Speed of transition
-        new_window : 1,             // Image links open in new window/tab
-        pause_hover : 0,            // Pause slideshow on hover
-        keyboard_nav : 0,           // Keyboard navigation on/off
-        performance : 0,            // 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
-        image_protect : 1,          // Disables image dragging and right click with Javascript
-        min_width : 0,              // Min width allowed (in pixels)
-        min_height : 0,             // Min height allowed (in pixels)
-        vertical_center : 1,        // Vertically center background
-        horizontal_center : 1,      // Horizontally center background
-        fit_always : 0,             // Image will never exceed browser width or height (Ignores min. dimensions)
-        fit_portrait : 1,           // Portrait images will not exceed browser height
-        fit_landscape : 0,          // Landscape images will not exceed browser width					
-        slide_links : 'blank',      // Individual links for each slide (Options: false, 'num', 'name', 'blank')
-        thumb_links : 1,            // Individual thumb links for each slide
-        thumbnail_navigation : 1,   // Thumbnail navigation
-        slides : [                  // Slideshow Images
-            {image : url_path + '/content/image/background/image_1.jpg'},
-            {image : url_path + '/content/image/background/image_2.jpg'},
-            {image : url_path + '/content/image/background/image_3.jpg'},  
-            {image : url_path + '/content/image/background/image_4.jpg'},  
-            {image : url_path + '/content/image/background/image_5.jpg'},  
-            {image : url_path + '/content/image/background/image_6.jpg'},  
-            {image : url_path + '/content/image/background/image_7.jpg'},  
-            {image : url_path + '/content/image/background/image_8.jpg'},  
-            {image : url_path + '/content/image/background/image_9.jpg'},  
-            {image : url_path + '/content/image/background/image_10.jpg'},  
-            {image : url_path + '/content/image/background/image_11.jpg'}
-        ],
-        progress_bar : 1,           // Timer for each slide							
-        mouse_scrub : 0
-    });
-}
-function stopBackground(){
-    $(".load-item").hide();
-    $.supersized({stop_loop : 1});
-}
