af1ad46f2b2bc9e1792eb6286746fca44243c25d
Pentor Saravudecha <p.saravudecha@gmail.com>
2014-08-08 11:35:53 +0700

    Create UPDATE_Config_20140808.sql

---
+++ SQL scripts/UPDATE_Config_20140808.sql
@@ -0,0 +1,4 @@
+UPDATE `Config` SET `configValue`='For detailed information on our shipping policies, please visit our <a href="policy#shipping">Shipping Information page.</a>' WHERE `configName`='FAQ_answer-2';
+UPDATE `Config` SET `configValue`='For detailed information on our returns and exchange policies, please visit our <a href="policy#return">Returns and Exchanges page.</a>' WHERE `configName`='FAQ_answer-3';
+UPDATE `Config` SET `configValue`='No, definitely not.' WHERE `configName`='FAQ_answer-5';
+UPDATE `Config` SET `configValue`='International orders will be dealt with on a case by case basis as shipping costs vary between different weights, dimensions and countries. If you live outside the Thailand and are interested in something, please let contact info@sundaydogshop.com' WHERE `configName`='FAQ_answer-6';
