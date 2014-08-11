d7360a9a5efe9349d143f4959624dab193e6d42a
p.saravudecha@gmail.com <p.saravudecha@gmail.com>
2014-08-11 16:22:19 +0700

    Feedback fix final3

---
+++ SQL scripts/UPDATE_ProductColor_20140811.sql
@@ -0,0 +1,11 @@
+DELETE FROM `Product_Color` WHERE `item_no`='40009';
+INSERT INTO `Product_Color`(`item_no`, `color`) VALUES ('40009','Red/White');
+INSERT INTO `Product_Color`(`item_no`, `color`) VALUES ('40009','Grey/Red');
+INSERT INTO `Product_Color`(`item_no`, `color`) VALUES ('40009','Grey/Blue');
+INSERT INTO `Product_Color`(`item_no`, `color`) VALUES ('40009','Orange/Blue');
+INSERT INTO `Product_Color`(`item_no`, `color`) VALUES ('40009','Orange/Red');
+
+DELETE FROM `Product_Color` WHERE `item_no`='40001';
+INSERT INTO `Product_Color`(`item_no`, `color`) VALUES ('40001','Pink');
+INSERT INTO `Product_Color`(`item_no`, `color`) VALUES ('40001','Green');
+INSERT INTO `Product_Color`(`item_no`, `color`) VALUES ('40001','Orange');
--- js/views/policy.js
+++ js/views/policy.js
@@ -3,4 +3,5 @@ var section = url.split("#");
 if (section.length > 1)
     changingSlide = setTimeout(function(){
         location.href = "#" + section[1];
+        window.scrollBy(0, -175);
     }, 3000);
